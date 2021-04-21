1c2fb23f270860dff1b36c6756ec102155b44f79
Somnams <3244345132@qq.com>
2021-04-21 20:06:16 +0800

    🚨 User Model

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
+
+#b
+db = SQLAlchemy()
+# flask-migrate plugin
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -8,6 +15,8 @@ def create_app(config_class=Config):
 
     # CORS
     CORS(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
